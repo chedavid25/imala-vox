ee1f7af8cd4c7de30563bd081a77506d06c1bfa8
chedavid25 <davidtello2502@gmail.comgit config --global user.email davidtello2502@gmail.comgit config --global user.email davidtello2502@gmail.com>
2026-05-01 11:04:09 -0300

    agregado de catalogo

--- functions/.puppeteerrc.cjs
+++ functions/.puppeteerrc.cjs
@@ -4,7 +4,6 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Indica a Puppeteer que descargue Chrome en la carpeta de la función
-  // Esto asegura que el navegador viaje con el código a Google Cloud.
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, 'node_modules', '.puppeteer_cache'),
 };
